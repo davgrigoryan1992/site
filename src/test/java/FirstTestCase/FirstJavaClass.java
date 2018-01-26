f32eb95d66ccda0ed4396885c22a491c57d5dd3c
Davit Grigoryan <david_grigoryan@epam.com>
2018-01-26 12:35:13 +0400

    second commit

--- src/test/java/FirstTestCase/FirstJavaClass.java
+++ src/test/java/FirstTestCase/FirstJavaClass.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
-
+// added some comment
 public class FirstJavaClass {
 
     WebDriver driver;
